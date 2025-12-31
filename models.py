015503c7370e6e655af29892aa0a6fb40859c97d
Nguyễn Hữu Dũng <huudung5c@gmail.com>
2025-12-31 13:05:17 +0700

    Change created_at to use server_default for timestamp

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
-from main import db
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class SearchLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     query = db.Column(db.String(255), nullable=False)
     ip_hash = db.Column(db.String(64))
     user_agent = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
