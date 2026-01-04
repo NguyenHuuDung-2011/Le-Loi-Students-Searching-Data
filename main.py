0e3504211fd1219091c1f4b5633d5be12ec951f0
NguyenHuuDung-2011 <huudung5c@gmail.com>
2026-01-04 10:54:02 +0700

    Print logs whenever users search data

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import json
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
 import os
 import hashlib
-from models import SearchLog, db
 
 app = Flask(__name__)
-
-# Kết nối database (PostgreSQL trên Render)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db.init_app(app)
 
 def hash_ip(ip):
     if not ip:
@@ -28,24 +20,11 @@ def normalize(text: str) -> str:
 def home():
     query = request.form.get("name", "")
 
-    if request.method == "POST" and query and len(query) >= 3:
-        raw_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
-        ip = raw_ip.split(",")[0].strip()
-
-        ua = request.headers.get("User-Agent")
-
-        log = SearchLog(
-            query=query,
-            ip_hash=hash_ip(ip),
-            user_agent=ua
-        )
-        db.session.add(log)
-        db.session.commit()
-        
     results = []
 
     if request.method == "POST":
         keyword = normalize(request.form.get("name", ""))
+        print(f"Search query: {keyword}")
 
         if keyword:
             results = [
@@ -56,6 +35,4 @@ def home():
     return render_template('index.html', results=results)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all() 
     app.run(port=5000, debug=True)
